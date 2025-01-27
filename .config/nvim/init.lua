4721558aac7c5726b10a020585cfd341c7f1b5ef
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2025-01-27 09:21:10 +0000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,21 +13,32 @@ vim.opt.shiftwidth = 4 					-- number of spaces to use for indent
 vim.expandtab = false 					-- don't use tabs for indent
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", 
-    lazypath,
-  }):wait()
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.opt.runtimepath:prepend(lazypath)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
 
 vim.keymap.set('n', '<C-t>', '<cmd>:Telescope file_browser<cr>')
 vim.keymap.set('n', '<C-f>f', '<cmd>:Telescope grep_string<cr>')
