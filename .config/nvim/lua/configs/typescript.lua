30f5ea0417361e164f271cab6c6b92f4967f5a79
Stefanos Kouroupis <stefanos.kouroupis@cloudcall.com>
2025-12-03 09:03:07 +0000

    Refactor TypeScript LSP configuration

--- .config/nvim/lua/configs/typescript.lua
+++ .config/nvim/lua/configs/typescript.lua
@@ -1,26 +1,21 @@
 -- typescript
-local status, nvim_lsp = pcall(require, "lspconfig")
-if not status then
-	return
-end
-
-nvim_lsp.ts_ls.setup({
-	on_attach = on_attach,
-	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-	cmd = { "typescript-language-server", "--stdio" },
+vim.lsp.config("ts_ls", {
+        on_attach = on_attach,
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" },
 })
 
 local status, prettier = pcall(require, "prettier")
 if not status then
-	return
+        return
 end
 
 prettier.setup({
-	bin = "prettierd",
-	filetypes = {
-		"css",
-		"javascript",
-		"typescript",
-		"json",
-	},
+        bin = "prettierd",
+        filetypes = {
+                "css",
+                "javascript",
+                "typescript",
+                "json",
+        },
 })
